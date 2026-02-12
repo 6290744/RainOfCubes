c26e6ddef0f94a35d938a561806a4b6f43319ae2
ERisunkov <6290744@gmail.com>
2026-02-12 22:01:13 +0100

    Added lifetime coroutine, changed collision check parameters

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,37 +1,52 @@
 using UnityEngine;
 using System;
+using System.Collections;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
 
 public class Cube : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer;
+    [SerializeField] private int _minimalCubeLifetime = 2;
+    [SerializeField] private int _maximalCubeLifeTime = 5;
     
     private Color _defaultColor;
-    private bool _isTouched;
-    private string _aimTag = "Platform";
+    private bool _isPlatformTouched;
     
-    public event Action<Cube> OnTouched;
+    public event Action<Cube> PlatformTouched;
+    public event Action<Cube> LifetimeEnded;
 
     public void SetDefaultState()
     {
-        _isTouched = false;
+        _isPlatformTouched = false;
         _renderer.material.color = _defaultColor;
     }
     
     private void Awake()
     {
+        _isPlatformTouched = false;
         _defaultColor = _renderer.material.color;
-        _isTouched = false;
     }
     
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag(_aimTag) && _isTouched == false)
+        if (_isPlatformTouched == false && collision.gameObject.TryGetComponent<Platform>(out _))
         {
-            _isTouched = true;
+            _isPlatformTouched = true;
             
-            OnTouched?.Invoke(this);
+            StartCoroutine(LifetimeStopwatch());
+            
+            PlatformTouched?.Invoke(this);
         }
+    }
+    
+    private IEnumerator LifetimeStopwatch()
+    {
+        int time = Random.Range(_minimalCubeLifetime, _maximalCubeLifeTime);
+        
+        yield return new WaitForSeconds(time);
+        
+        LifetimeEnded?.Invoke(this);
     }
 }
